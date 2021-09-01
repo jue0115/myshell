e1d9d4cf1982bde77366af1f3fdabf8b1400c1e1
jue0115 <46835138+jue0115@users.noreply.github.com>
2021-09-01 14:40:50 +0800

    Create ssh.sh

---
+++ ssh.sh
@@ -0,0 +1,6 @@
+sed 's/#PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config -i
+sed 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config -i
+sed 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config -i
+sed 's/#PermitEmptyPasswords no/PermitRootLogin yes/g' /etc/ssh/sshd_config -i
+sed 's/PermitRootLogin no/PermitRootLogin yes/g' /etc/ssh/sshd_config -i
+sudo systemctl restart sshd
