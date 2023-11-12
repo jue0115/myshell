82bf650b0944b32dcbf9bf287646ea16389bcb11
jue0115 <46835138+jue0115@users.noreply.github.com>
2023-11-12 12:27:46 +0800

    update

--- speedtest.sh
+++ speedtest.sh
@@ -3,9 +3,9 @@ bit=`uname -m`
 
 function speedtest() {
 	if [[ ${bit} == "x86_64" ]]; then
-      wget -O speedtest https://raw.githubusercontent.com/jue0115/Shell/main/speedtest_amd64 && chmod +x speedtest && ./speedtest
+      wget -O speedtest https://raw.githubusercontent.com/jue0115/myshell/main/speedtest_amd64 && chmod +x speedtest && ./speedtest
 	elif [[ ${bit} == "aarch64" ]]; then
-      wget -O speedtest https://raw.githubusercontent.com/jue0115/Shell/main/speedtest_arm64 && chmod +x speedtest && ./speedtest
+      wget -O speedtest https://raw.githubusercontent.com/jue0115/myshell/main/speedtest_arm64 && chmod +x speedtest && ./speedtest
 	fi
 }
 
